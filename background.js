78c639a110bf7ed9c9e649ad92550131a7b9e127
Andrew Armbruster <andrew.armbruster@gmail.com>
2015-04-25 13:55:37 -0500

    Added Slideshow example code and MediaGallery example code to build off of
    Added settings.json but may not need it

--- background.js
+++ background.js
@@ -13,6 +13,26 @@ chrome.app.runtime.onLaunched.addListener(function(launchData) {
     {
       id: 'mainWindow',
       bounds: {width: 1920, height: 1080}
-    }
+    },
+    function(app_win) {
+    		app_win.contentWindow.__MGA__bRestart = false;
+    	}
   );
+  
+  console.log("app launched");
+  
+});
+
+
+// Use the runtime event listeners to set a window property indicating whether the
+// app was launched normally or as a result of being restarted
+
+chrome.app.runtime.onRestarted.addListener(function() {
+    chrome.app.window.create('page.html', 
+    	{innerBounds: {width:900, height:600, minWidth:900, maxWidth: 900, minHeight:600, maxHeight: 600}, id:"MGExp"}, 
+    	function(app_win) {
+    		app_win.contentWindow.__MGA__bRestart = true;
+    	}
+    );
+    console.log("app restarted");
 });
