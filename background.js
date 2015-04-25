4708bd32ecbf859ed775fa52b2f5ef4c7ec45dee
Andrew Armbruster <andrew.armbruster@gmail.com>
2015-04-24 20:11:15 -0500

    Added power permission and diplay awake request

--- background.js
+++ background.js
@@ -5,11 +5,14 @@
  * @see http://developer.chrome.com/apps/app.window.html
  */
 chrome.app.runtime.onLaunched.addListener(function(launchData) {
+  
+  chrome.power.requestKeepAwake('display');
+  
   chrome.app.window.create(
     'index.html',
     {
       id: 'mainWindow',
-      bounds: {width: 800, height: 600}
+      bounds: {width: 1920, height: 1080}
     }
   );
 });
